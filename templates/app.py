5e7770a79b0fb87f0516ef1a0ec5e232ee34e71e
hyan0217 <hyan0217@live.com>
2022-03-30 16:32:40 -0400

    Flask installation

--- templates/app.py
+++ templates/app.py
@@ -1,4 +1,24 @@
 from flask import Flask
 from flask import render_template
 from datetime import datetime
-from . import app
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")

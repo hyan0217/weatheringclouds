d7fe0125fe5309b3f6345dfea0e219ad2f7a90ab
hyan0217 <hyan0217@live.com>
2022-03-31 09:53:42 -0400

    Added heroku

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+from . import app
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# @app.route("/about/")
+# def about():
+#     return render_template("about.html")
+
+
+# @app.route("/contact/")
+# def contact():
+#     return render_template("contact.html")
+
+
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello_there(name=None):
+#     return render_template(
+#         "hello_there.html",
+#         name=name,
+#         date=datetime.now()
+#     )
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")

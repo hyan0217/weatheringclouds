f7b857eea6a58744e024401dbb5159da02cd7667
hyan0217 <hyan0217@live.com>
2022-04-01 11:19:34 -0400

    Installed databases and flask-wtf

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 import os
 from flask import Flask, render_template, flash, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
 import gunicorn
 from datetime import datetime
 from helpers import apology
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.text, nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 @app.route("/")
--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import urllib.parse
-
 from flask import redirect, render_template, request, session
 import gunicorn
 from functools import wraps
@@ -22,18 +21,19 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-# def login_required(f):
-#     """
-#     Decorate routes to require login.
+def login_required(f):
+    """
+    Decorate routes to require login.
 
-#     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-#     """
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         if session.get("user_id") is None:
-#             return redirect("/login")
-#         return f(*args, **kwargs)
-#     return decorated_function
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 def usd(value):
     """Format value as USD."""

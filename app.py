9f3074ee48d4358525d4daec4d65d753727bef29
hyan0217 <hyan0217@live.com>
2022-04-01 18:13:31 -0400

    update

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ if __name__ == '__main__':
 #     )
 
 
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+# @app.route("/api/data")
+# def get_data():
+#     return app.send_static_file("data.json")

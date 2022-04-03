1a93369a7cb356c3c635b3187dbefd8e4f804902
hyan0217 <hyan0217@live.com>
2022-04-03 18:41:07 -0400

    added Zillow api

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import http.client
 import os
 from flask import Flask, render_template, flash, redirect, request, session
 import gunicorn
 from datetime import datetime
 from helpers import apology
+import http.client
 
 app = Flask(__name__)
 
@@ -38,3 +40,18 @@ if __name__ == '__main__':
 # @app.route("/api/data")
 # def get_data():
 #     return app.send_static_file("data.json")
+
+conn = http.client.HTTPSConnection("zillow-com1.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Host': "zillow-com1.p.rapidapi.com",
+    'X-RapidAPI-Key': "5d5d38275amsh8a0dad41cfbbabcp1d9de2jsn5bb539b8005f"
+}
+
+conn.request(
+    "GET", "/propertyExtendedSearch?location=santa%20monica%2C%20ca&home_type=Houses", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

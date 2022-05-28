1e53cb672356c53ac0d96d841c16baa53f953b90
hyan0217 <hyan0217@live.com>
2022-05-27 21:29:03 -0400

    Update

--- applications/config.py
+++ applications/config.py
@@ -1,8 +1,8 @@
-# import os
+import os
 
 
-# class Config:
-#     # Secret Key for Postgresql stored local environment
-#     SECRET_KEY = os.environ.get("SECRET_KEY")
-#     # Connecting to the Database stored local environment
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+class Config:
+    # Secret Key for Postgresql stored local environment
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    # Connecting to the Database stored local environment
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
